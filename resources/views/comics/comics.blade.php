7cc1bc294a2ff68e48fe22d04dd104eacc57f5f2
Giulio998 <giulio_favia@hotmail.it>
2024-05-30 10:08:49 +0200

    Refinement

--- resources/views/comics/comics.blade.php
+++ resources/views/comics/comics.blade.php
@@ -1,22 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
-    <div class="col-auto ms-auto mb-3">
-      <a href="{{ route('comics.create') }}" class="btn btn-primary">Nuovo Fumetto</a>
-    </div>
-    <div class="row">
-      @foreach ($comics as $comic)
-        <div class="col-4">
-          <ul>
-            <li><a href="{{ route('comics.show', $comic) }}">{{ $comic->title }}</a></li>
-            <li><img src="{{$comic->thumb}}" alt=""></li>
-            <li>{{ $comic->description}}</li>
-            <li>{{ $comic->type}}</li>
-          </ul>
-        </div>
-      @endforeach
-    </div>
+<div class="container">
+  <div class="col-auto ms-auto mb-3">
+    <a href="{{ route('comics.create') }}" class="btn btn-primary">Nuovo Fumetto</a>
   </div>
+  <div class="row">
+    @foreach ($comics as $comic)
+    <div class="col-4">
+      <div class="card" style="width: 18rem;">
+      <img src="{{$comic->thumb}}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title"><a href="{{ route('comics.show', $comic) }}">{{ $comic->title }}</a></h5>
+        <p class="card-text">{{ \Illuminate\Support\Str::limit($comic->description,$limit = 150)}}</p>
+      </div>
+      <div class="card-footer">
+        <p>{{ $comic->type}}</p>
+      </div>
+      </div>
+    </div>
+  @endforeach
+  </div>
+</div>
 
 @endsection
